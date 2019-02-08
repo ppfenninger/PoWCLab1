d2817997e0929b3612b3ac47134fbf80b384d5a9
ppfenninger <paige.pfenninger@students.olin.edu>
2019-02-07 19:29:53 -0500

    hopefully fixed git

--- read_qpsk_file.m
+++ read_qpsk_file.m
@@ -1,25 +1,46 @@
 % Open the file containing the received samples
-f2 = fopen('rx2.dat', 'rb');
-
+f2 = fopen('rx4.dat', 'rb');
+ 
 % read data from the file
 tmp = fread(f2, 'float32');
-
+ 
 % close the file
 fclose(f2);
-
-
+ 
+ 
 % since the USRP stores the data in an interleaved fashion
 % with real followed by imaginary samples 
 % create a vector of half the length of the received values to store the
 % data. Make every other sample the real part and the remaining samples the
 % imaginary part
-y = zeros(length(tmp)/2,1);
-y = tmp(1:2:end)+j*tmp(2:2:end);
-
-
+% y = zeros(length(tmp)/2,1);
+y = tmp(1:2:end)+1i*tmp(2:2:end);
+ 
+startSignal = 0;
+endSignal = 0;
+ 
+for i = 21:length(y)
+    if y(i) > 0.2 && startSignal == 0
+        startSignal = i;
+    end
+    
+    if mean(abs(y(i-20:i))) < 0.005 && endSignal == 0 && startSignal ~= 0
+        endSignal = i - 20;
+        break;
+    end
+end
+ 
+cleanData = y(startSignal:endSignal);
+ 
+save('cleanData.mat', 'cleanData');
+ 
+ 
+plot(real(y(startSignal:end)), imag(y(startSignal:end)), '*');
+ 
+ 
 % to visualize, plot the real and imaginary parts separately
 return;
-subplot(211)
-stem(real(y));
-subplot(212)
-stem(imag(y));
+% subplot(211)
+% stem(real(y));
+% subplot(212)
+% stem(imag(y));
